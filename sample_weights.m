479f6935bddcc187643a82980b1101750d97c055
Andrew Buck <buckar@missouri.edu>
2018-07-26 18:25:40 -0500

    Add example script to create a mosaic of 3D shapes

--- sample_weights.m
+++ sample_weights.m
@@ -1,4 +1,16 @@
-function W = sample_weights(num_samples, num_features)
+function W = sample_weights(n, d)
+%% SAMPLE_WEIGHTS Returns a uniformly distributed set of normalized weights.
+%
+%  INPUT:
+%      n : Number of independent weight vectors to return
+%      d : Number of feature dimensions
+%
+%  OUTPUT:
+%      W : <n x d> matrix of normalized weight vectors
+%
+%  Author:
+%      Andrew Buck (7/26/2018)
+%%
 
-W = rand(num_samples, num_features-1);
-W = diff([zeros(num_samples,1), sort(W,2), ones(num_samples,1)], 1, 2);
+W = rand(n, d-1);
+W = diff([zeros(n,1), sort(W,2), ones(n,1)], 1, 2);
